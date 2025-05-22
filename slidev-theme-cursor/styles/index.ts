693f73f054a65b44c84324a759d70a488d12b104
sanjeed5 <msanjeed5@gmail.com>
2025-05-22 14:52:48 +0530

    create theme

---
+++ slidev-theme-cursor/styles/index.ts
@@ -0,0 +1,6 @@
+// inherit from base layouts, remove it to get full customizations
+import '@slidev/client/styles/layouts-base.css'
+import './layout.css'
+
+// You can add more global CSS files here
+// e.g. import './another-style.css'
